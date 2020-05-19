2faff2fbac33e12fcacebaeee8fec9acd4daec85
Suvineet Singh <suvineet1998@gmail.com>
2020-05-19 15:56:57 +0100

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by Suvineet Singh on 18/05/2020.
+//  Copyright © 2020 Suvineet Singh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+
+  std::cout << "Welcome to Bulls and Cows!" << std::endl;
+  return 0;
+}
